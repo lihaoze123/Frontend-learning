e44a9243dcc271f4af89ae38985f0749544f2bb3
chumeng <2595248810@qq.com>
2025-02-05 23:28:11 +0800

    feat: add error handling and not-found page for invoices

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -27,10 +27,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -47,27 +51,40 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-  `;
+  try {
+    await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+  } catch (error) {
+    console.log(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  throw Error("Failed to Delete Invoice");
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.log(error);
+  }
   revalidatePath("/dashboard/invoices");
 }
 
 export async function copyInvoice(id: string) {
-  await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      SELECT customer_id, amount, status, date
-      FROM invoices
-      WHERE id = ${id}
-  `;
+  try {
+    await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            SELECT customer_id, amount, status, date
+            FROM invoices
+            WHERE id = ${id}
+        `;
+  } catch (error) {
+    console.log(error);
+  }
   revalidatePath("/dashboard/invoices");
 }
